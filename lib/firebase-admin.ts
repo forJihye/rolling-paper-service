ddb3ea3912b2a9f9d1ff63b66c128f0558f8200f
byjihye <byjihye@hashsnap.net>
2022-11-29 11:53:17 +0900

    @lowfront/firebase-adapter

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,18 +1,36 @@
-import admin, { credential, ServiceAccount } from 'firebase-admin';
+import admin, { ServiceAccount } from 'firebase-admin';
 import { App } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import serviceAccount from 'serviceAccountKey.json';
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: credential.cert(serviceAccount as ServiceAccount)
-  });  
+    // https://firebase.google.com/docs/admin/setup#initialize-sdk
+    credential: admin.credential.cert({
+      type: process.env.FIREBASE_ADMIN_TYPE,
+      project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+      client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+      token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+    } as ServiceAccount),
+  });
 }
-
-const app = admin.apps[0] as App;
+const app = admin.apps[0] as App
 const db = getFirestore(app);
-
 export {app, db};
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: credential.cert(serviceAccount as ServiceAccount)
+//   });  
+// }
+// const app = admin.apps[0] as App;
+// const db = getFirestore(app);
+// export {app, db};
 
 
 
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import KakaoProvider from "next-auth/providers/kakao";
-import { db } from "lib/firebase-client";
-import { FirebaseAdapter } from "@next-auth/firebase-adapter";
-import { collection, query, getDoc, where, limit, doc, getDocs, addDoc, updateDoc, deleteDoc, runTransaction } from "firebase/firestore";
-// import { FirestoreAdapter } from "@lowfront/firebase-adapter";
+import { FirestoreAdapter } from "@lowfront/firebase-adapter";
+import { db } from "lib/firebase-admin";
 
 export default NextAuth({
   providers: [
@@ -25,22 +23,7 @@ export default NextAuth({
       clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
     })
   ],
-  // adapter: FirestoreAdapter(db),
-  // FIXME: toDate ERROR https://stackoverflow.com/questions/69876727/next-auth-google-auth-firebase-adapter
-  adapter: FirebaseAdapter({
-    db,
-    collection,
-    query,
-    getDocs,
-    where,
-    limit,
-    doc,
-    getDoc,
-    addDoc,
-    updateDoc,
-    deleteDoc,
-    runTransaction
-  }),
+  adapter: FirestoreAdapter(db),
   secret: process.env.SESSION_SECRET,
   callbacks: {
     async session({ session, user, token }) {
@@ -56,4 +39,62 @@ export default NextAuth({
       return token;
     }
   }
-})
+});
+
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import KakaoProvider from "next-auth/providers/kakao";
+// import { db } from "lib/firebase-client";
+// import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+// import { collection, query, getDoc, where, limit, doc, getDocs, addDoc, updateDoc, deleteDoc, runTransaction } from "firebase/firestore";
+// export default NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//       authorization: {
+//         params: {
+//           prompt: "consent",
+//           access_type: "offline",
+//           response_type: "code",
+//           scope: "openid email profile"
+//         }
+//       }
+//     }),
+//     KakaoProvider({
+//       clientId: process.env.KAKAO_CLIENT_ID as string,
+//       clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+//     })
+//   ],
+//   // adapter: FirestoreAdapter(db),
+//   // FIXME: toDate ERROR https://stackoverflow.com/questions/69876727/next-auth-google-auth-firebase-adapter
+//   adapter: FirebaseAdapter({
+//     db,
+//     collection,
+//     query,
+//     getDocs,
+//     where,
+//     limit,
+//     doc,
+//     getDoc,
+//     addDoc,
+//     updateDoc,
+//     deleteDoc,
+//     runTransaction
+//   }),
+//   secret: process.env.SESSION_SECRET,
+//   callbacks: {
+//     async session({ session, user, token }) {
+//       return {
+//         ...session,
+//         user
+//       };
+//     },
+//     async jwt ({token, user, account, profile, isNewUser}) {
+//       if (account) { // 로그인 직후 토큰에 대한 OAuth access_token 유지
+//         token.accessToken = account.access_token
+//       }
+//       return token;
+//     }
+//   }
+// })
