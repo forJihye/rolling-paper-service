8b7d84ea38f342ef6bc3efad27c00ec2e741038a
byjihye <byjihye@hashsnap.net>
2023-03-27 17:41:00 +0900

    Update Login page Style

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,11 +3,13 @@ import { signIn } from "next-auth/react";
 import Layout from "@/components/Layout"
 
 const Login: NextPage<{}> = ({}) => {
-  return <Layout flexCenter={true}>    
-    <h1 className='text-3xl text-pink tracking-tighter mb-7'>Login</h1>
-    <div className='w-full text-center px-10'>
-      <a onClick={(ev) => signIn('google', {callbackUrl: '/paper'})} className="mb-5 block w-full py-4 px-12 mx-auto text-center rounded-full text-gray-500 hover:text-pink neumorphism hover:shadow-inset cursor-pointer">Google 계정 로그인</a>
-      <a onClick={(ev) => signIn('kakao', {callbackUrl: '/paper'})}  className="block w-full py-4 px-12 mx-auto text-center rounded-full text-gray-500 hover:text-pink neumorphism hover:shadow-inset cursor-pointer">카카오 계정 로그인</a>
+  return <Layout>    
+    <div className="pt-14 text-center">
+      <h1 className='text-3xl text-pink tracking-tighter mb-7'>Login</h1>
+      <div className='w-full text-center px-10'>
+        <a onClick={(ev) => signIn('google', {callbackUrl: '/paper'})} className="mb-5 block w-full py-4 px-12 mx-auto text-center rounded-full text-gray-500 hover:text-pink neumorphism hover:shadow-inset cursor-pointer">Google 계정 로그인</a>
+        <a onClick={(ev) => signIn('kakao', {callbackUrl: '/paper'})}  className="block w-full py-4 px-12 mx-auto text-center rounded-full text-gray-500 hover:text-pink neumorphism hover:shadow-inset cursor-pointer">카카오 계정 로그인</a>
+      </div>
     </div>
   </Layout>
 }
