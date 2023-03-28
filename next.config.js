42fc59d628cd07f1515823f43128c8d230fcb04d
byjihye <byjihye@hashsnap.net>
2023-03-28 10:06:44 +0900

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path')
-const tsconfigPaths = require('vite-tsconfig-paths');
-
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
   api: {
     bodyParser: {
       sizeLimit: '1mb',
     },
-  },
-  plugins: [tsconfigPaths()],
+  }
 }
 
 module.exports = nextConfig
